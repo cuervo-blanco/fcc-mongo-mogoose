3aa32335f028235acd2b722b31ad0ec3fca68440
cuervo-blanco <jaios.228@gmail.com>
2023-12-28 13:26:26 -0500

    fix: Schema typo

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-const Scheme = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const personSchema = new Schema({
 	name: { type: String, required: true },
